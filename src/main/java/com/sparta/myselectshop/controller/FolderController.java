39b3d8c051eee5647082ef36b1b03ab5d4d6d217
Kim-Jong-Gyu <whdrb2643@gmail.com>
2023-11-30 17:44:40 +0900

    @ExceptionHandler 적용

--- src/main/java/com/sparta/myselectshop/controller/FolderController.java
+++ src/main/java/com/sparta/myselectshop/controller/FolderController.java
@@ -2,9 +2,12 @@ package com.sparta.myselectshop.controller;
 
 import com.sparta.myselectshop.dto.FolderRequestDto;
 import com.sparta.myselectshop.dto.FolderResponseDto;
+import com.sparta.myselectshop.exception.RestApiException;
 import com.sparta.myselectshop.security.UserDetailsImpl;
 import com.sparta.myselectshop.service.FolderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,5 +28,18 @@ public class FolderController {
     @GetMapping("/folders")
     public List<FolderResponseDto> getFolders(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         return folderService.getFolders(userDetails.getUser());
+    }
+
+    // Controller에서 예외가 발생하면 호출이 되고 해당 예외를 처리를 해준다.
+    // AOP 기반으로 작동해서 일일이 적용 시킬 필요 없다.
+    @ExceptionHandler({IllegalArgumentException.class})
+    public ResponseEntity<RestApiException> handleException(IllegalArgumentException ex) {
+        RestApiException restApiException = new RestApiException(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(
+                // HTTP body
+                restApiException,
+                // HTTP status code
+                HttpStatus.BAD_REQUEST
+        );
     }
 }
